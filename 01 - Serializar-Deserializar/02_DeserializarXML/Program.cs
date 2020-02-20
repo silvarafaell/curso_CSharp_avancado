a634fdf062a77cc5fd4f53f5fa4e857541d3db97
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2020-02-20 11:05:40 -0300

    Aula 4 - Deserializar no formato XML

---
+++ 01 - Serializar-Deserializar/02_DeserializarXML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _00_Biblioteca;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace _02_DeserializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"C:\Fitbank\Projetos\curso_CSharp_avancado\01_SerializarXML.xml");
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+            Usuario usuario = (Usuario) serializador.Deserialize(stream);
+            Console.WriteLine("Usuario(Nome): {0}, CPF: {1} e Email: {2}",usuario.Nome,usuario.CPF,usuario.Email);
+            Console.ReadKey();
+
+        }
+    }
+}
