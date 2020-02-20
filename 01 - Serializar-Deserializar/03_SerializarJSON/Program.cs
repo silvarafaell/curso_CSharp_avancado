76d27899ea9f917821c98190b62f941f747bfd8f
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2020-02-20 11:56:45 -0300

    Aula 5 - Serializar no formato JSON

---
+++ 01 - Serializar-Deserializar/03_SerializarJSON/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using _00_Biblioteca;
+using System.IO;
+
+namespace _03_SerializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "Gabriela Aguiar de Oliveira", CPF = "060.133.293-89", Email = "gabrielaaguiar21@outlook.com"};
+
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+
+            string stringObjSerializado = serializador.Serialize(usuario);
+
+            StreamWriter sw = new StreamWriter(@"C:\Fitbank\Projetos\curso_CSharp_avancado\02_SerializarJSON.json");
+            sw.WriteLine(stringObjSerializado);
+            sw.Close();
+        }
+    }
+}
